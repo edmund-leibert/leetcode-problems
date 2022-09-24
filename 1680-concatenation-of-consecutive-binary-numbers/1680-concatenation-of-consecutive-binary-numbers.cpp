b0db6881dc643f38eaafe3a999e30446c2bb4e44
Edmund Leibert III <edmund.leibert@gmail.com>
2022-09-23 23:23:09 -0700

    dev: final solution

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -2,42 +2,21 @@
 #include <string>
 #include <cmath>
 
-/* std::string decToBin(int decimal) {
-  std::string binary;
-  while(decimal > 0) {
-    binary = std::to_string(decimal % 2) + binary;
-    decimal /= 2;
+  int binLength(int binary) {  // Most optimized way
+    int LeadingZeros = __builtin_clz(binary);
+    return 32 - LeadingZeros;
   }
-
-  return binary;
-} */
   
-/* int binToDec(std::string binary) {
-    int result = 0;
-    std::string binDigit = "";
-    int binExponent = 0;
-    for(int i = binary.size() - 1; i >= 0; i--) {
-      binDigit = binary.at(i);
-      result = result + std::stoi(binDigit) * (std::pow(2,binExponent));
-      binExponent++;
-    }  
-    return result;
-  } */
-
-int concatenatedBinary(int n) {
-  std::string nCountBin = "";
-
-  // iterate  through n and convert each integer to a binary
-  //    *Going need a binary to decimal function
-  for(int nCount = 1; nCount <= n; nCount++) {
-    //  *concaenate the new binary string to final binary string  
-    nCountBin = nCountBin + decToBin(nCount);
-  } 
-
-  // final binary string
-  // convert final binary string to answer
-  return binToDec(nCountBin);
-}
+  int concatenatedBinary(int n) {
+    long oneBinary = 1;
+    const int MODULO = 1e9 + 7;
+    for(int i = 2; i <= n; i++) {
+      oneBinary = ((oneBinary << binLength(i)) % MODULO) + i;
+      oneBinary = oneBinary % MODULO; 
+    }
+    
+    return oneBinary;
+  }
 
 int main() {
   std::cout << concatenatedBinary(3);
